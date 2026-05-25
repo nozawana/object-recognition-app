4e4ec516e822276945a0d7847ebb84ff8d53fa7e
nozawana <hiharudaishi@gmail.com>
2026-05-25 07:51:53 +0000

    カメラ設定

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { SmartphoneCamera } from './components/SmartphoneCamera';
+
+function App() {
+  return (
+    <>
+      <SmartphoneCamera onRunModel={(args) => {} } />
+    </>
+  )
+}
+
+export default App
